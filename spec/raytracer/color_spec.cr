c6fa01ca2fc21f2439c24177c1e9798e93413992
Jason Frey <fryguy9@gmail.com>
2023-09-21 23:06:23 -0400

    Add Color#+, -, *

--- spec/raytracer/color_spec.cr
+++ spec/raytracer/color_spec.cr
@@ -14,4 +14,37 @@ describe Raytracer::Color do
       c.should eq Raytracer::Color.new(1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0)
     end
   end
+
+  describe "#+" do
+    it "can add 2 colors" do
+      c1 = Raytracer::Color.new(0.9, 0.6, 0.75)
+      c2 = Raytracer::Color.new(0.7, 0.1, 0.25)
+      c3 = c1 + c2
+      c3.should eq Raytracer::Color.new(1.6, 0.7, 1.0)
+    end
+  end
+
+  describe "#-" do
+    it "can subtract 2 colors" do
+      c1 = Raytracer::Color.new(0.9, 0.6, 0.75)
+      c2 = Raytracer::Color.new(0.7, 0.1, 0.25)
+      c3 = c1 - c2
+      c3.should eq Raytracer::Color.new(0.2, 0.5, 0.5)
+    end
+  end
+
+  describe "#*" do
+    it "can multiply a color by a scalar" do
+      c1 = Raytracer::Color.new(0.2, 0.3, 0.4)
+      c2 = c1 * 2
+      c2.should eq Raytracer::Color.new(0.4, 0.6, 0.8)
+    end
+
+    it "can multiply 2 colors (Hadamard product)" do
+      c1 = Raytracer::Color.new(1, 0.2, 0.4)
+      c2 = Raytracer::Color.new(0.9, 1, 0.1)
+      c3 = c1 * c2
+      c3.should eq Raytracer::Color.new(0.9, 0.2, 0.04)
+    end
+  end
 end
--- src/raytracer/color.cr
+++ src/raytracer/color.cr
@@ -7,5 +7,21 @@ module Raytracer
         (green - other.green).abs < EPSILON &&
         (blue - other.blue).abs < EPSILON
     end
+
+    def +(other)
+      self.class.new(red + other.red, green + other.green, blue + other.blue)
+    end
+
+    def -(other)
+      self.class.new(red - other.red, green - other.green, blue - other.blue)
+    end
+
+    def *(value : Float64)
+      self.class.new(red * value, green * value, blue * value)
+    end
+
+    def *(other : self)
+      self.class.new(red * other.red, green * other.green, blue * other.blue)
+    end
   end
 end
